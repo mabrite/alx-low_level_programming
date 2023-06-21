93aa173d1add65a87fbb2071846ca4bd3038c69f
mabrite <mabrite39@gmail.com>
2023-06-21 09:44:46 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 2-main.c 2-print_alphabet_x10.c -o 2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,14 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <= 0)
+	while (i <= 9 )
 	{
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 
-	_putchar('\n');
 	}
+	_putchar('\n');
 	i++;
 	}
 }
