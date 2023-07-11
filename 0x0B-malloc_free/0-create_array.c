2d1c88afc340250902e1158fefd3d72e8f15e5a9
mabrite <mabrite39@gmail.com>
2023-07-11 10:46:13 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,21 +9,19 @@
  * Return: The array filled
  */
 char *create_array(unsigned int size, char c)
-{       
-        unsigned int i;
-        char *s;
-        
-        if (size == 0)
-                return (NULL);
-        s = malloc(size * sizeof(char));
-        
-        if (s == NULL)
-                return (NULL);
-                
-        for (i =0; i < size; i++)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL)
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
 	}
-
 	return (s);
 }
