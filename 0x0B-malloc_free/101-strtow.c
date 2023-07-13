e4fa762c3fe0b6e000352c58bd68d9bf4981add4
mabrite <mabrite39@gmail.com>
2023-07-12 21:40:06 -0700

    101 s

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,8 +4,8 @@
 
 /**
  * strtow - concatenates all the arguments of your program
- *@str: string
- *@av: arguments
+ * @str: string
+ *
  * Return: a pointer to a new string
  */
 char **strtow(char *str)
@@ -18,27 +18,31 @@ char **strtow(char *str)
 	j = 0;
 	i = 0;
 	count = 0;
-	if (*str == '\0' || str == NULL)
+	if (str == NULL || *str == '\0')
 		return (NULL);
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
 			w++;
 	}
+
 	p = (char **)malloc((w + 1) * sizeof(char *));
 	if (p == NULL)
 		return (NULL);
+
 	for (wordf = 0; str[wordf] && j <= w; wordf++)
 	{
 		count = 0;
 		if (str[wordf] != ' ')
 		{
-			for (i = wordf ; str[i] != '\0'; i++)
+			for (i = wordf; str[i] != '\0'; i++)
 			{
 				if (str[i] == ' ')
 					break;
 				count++;
 			}
+
 			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
 			if (*(p + j) == NULL)
 			{
@@ -50,6 +54,7 @@ char **strtow(char *str)
 				free(p);
 				return (NULL);
 			}
+
 			for (m = 0; wordf < i; wordf++)
 			{
 				p[j][m] = str[wordf];
@@ -59,6 +64,8 @@ char **strtow(char *str)
 			j++;
 		}
 	}
+
 	p[j] = NULL;
 	return (p);
 }
+
